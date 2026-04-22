ecc6e4abc2963eb4fdc579930a03a939548975d8
Maira-Ayaz <myrasheikh02@gmail.com>
2026-04-22 19:36:05 +0500

    to find prime_number

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,1 +1,10 @@
-print("is a prime number")
+#Check whether a number is a prime number using loops and conditionals.
+
+num = 29
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
