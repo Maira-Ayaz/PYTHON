c45dcebe2020bd88f2748c761e1ade3a0c79649f
Maira-Ayaz <myrasheikh02@gmail.com>
2026-04-30 21:20:00 +0500

    "finding even numbers"

---
+++ even_numbers.py
@@ -0,0 +1,14 @@
+nums = [1, 2, 3, 4, 6]
+total=0
+for i in range(0,len(nums)) :
+    if nums[i] % 2 == 0 : 
+        total = total + 1
+print ("even numbers are", total)
+
+num = [1, 3, 5]
+sum=0
+for a in range(0,len(num)) :
+    if num[a] % 2 == 0 : 
+        sum = sum + 1
+print ("even numbers are", sum)
+
